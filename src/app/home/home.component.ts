a9b8d16e44060ed11aeb0ae0a72169d91b3aad13
harenaMelanie <harenaharena77@gmail.com>
2022-05-15 15:45:11 +0300

    Responsive et icon bootstrap

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,9 +5,19 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit { 
+   home!:string;
+   about!:string;
+   livre!:string;
+   blog!:string;
+   contact!:string;
 
   ngOnInit(): void {
+    this.home='Home';
+    this.about='About';
+    this.livre='Livres';
+    this.blog='Blog';
+    this.contact='Contact';
   }
 
 }
