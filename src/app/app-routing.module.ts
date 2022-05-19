a0a248e5cb7b7d2864431c344affd53f43b54ea8
harenaMelanie <harenaharena77@gmail.com>
2022-05-19 14:49:42 +0300

    création de components et le blog

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { LivreComponent } from './livre/livre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'About',component:AboutComponent},
+  { path:'',component:HomeComponent},
+  { path:'Livre',component:LivreComponent},
+  { path:'Blog',component:BlogComponent},
+  { path:'Contact',component:ContactComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LivreComponent } from './livre/livre.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    HeaderComponent,
+    FooterComponent,
+    LivreComponent,
+    BlogComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
